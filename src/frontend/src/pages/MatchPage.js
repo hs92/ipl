6f7e205a67b38e21c10627cd46f3029e08f2709c
hs92 <himanshu84207@gmail.com>
2022-11-13 10:50:27 +0530

    Added Match Page changes

--- src/frontend/src/pages/MatchPage.js
+++ src/frontend/src/pages/MatchPage.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import {useParams} from 'react-router-dom';
+import {MatchDetailCard} from "../components/MatchDetailCard";
 
 export const MatchPage = () => {
+
+    const [matches, setMatches] = useState([]);
+    const {teamName, year} = useParams();
+
+    useEffect(() => {
+       const fetchResult = async () => {
+         const response = await fetch(`http://localhost:8080/team/${teamName}/matches?year=${year}`);
+         const res = await response.json();
+         setMatches(res);
+         console.log(res);
+       };
+       fetchResult();
+    });
+
     return (
         <div className="MatchPage">
-            <h1>Match Page</h1>
+            <h1>Match Page : {teamName}</h1>
+            {matches.length > 0 ?
+                matches.map(match => <MatchDetailCard key={match.id} teamName={teamName} match={match}/>)
+            : <h3>No info available</h3>}
         </div>
     );
 };
--- src/main/java/pp/fs/ipldashboard/controller/TeamController.java
+++ src/main/java/pp/fs/ipldashboard/controller/TeamController.java
@@ -1,12 +1,13 @@
 package pp.fs.ipldashboard.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import pp.fs.ipldashboard.model.Match;
 import pp.fs.ipldashboard.model.Team;
 import pp.fs.ipldashboard.repository.MatchRepository;
 import pp.fs.ipldashboard.repository.TeamRepository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -14,14 +15,25 @@ public class TeamController {
 
     private final TeamRepository teamRepository;
     private final MatchRepository matchRepository;
+
     public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
         this.teamRepository = teamRepository;
         this.matchRepository = matchRepository;
     }
+
     @GetMapping("/team/{teamName}")
     public Team getTeam(@PathVariable String teamName) {
         Team team = teamRepository.findByTeamName(teamName);
         team.setMatches(matchRepository.getLatestMatchByTeamName(teamName, 5));
         return team;
+    }
+
+    @GetMapping("/team/{teamName}/matches")
+    public List<Match> getMatches(@PathVariable String teamName, @RequestParam int year) {
+        LocalDate startDate = LocalDate.of(year, 1, 1);
+        LocalDate endDate = LocalDate.of(year + 1, 1, 1);
+        List<Match> matches = matchRepository.getMatchesByTeamBetweenDates(
+                teamName, startDate, endDate);
+        return matches;
     }
 }
